cf1b9f7ddc1ae0f6687b8d997e9899d69e63434b
Nour <nour.sehs.3@gmail.com>
2022-05-15 12:47:23 +0200

    Created seperate file for all functions and added header file

---
+++ text-editor-functions.h
@@ -0,0 +1,10 @@
+#ifndef TXTEDTR_H
+#define TXTEDTR_H
+
+const char ESC = 27;
+const char RETURN = 10;
+const char slash = 47;
+
+void choose_file(), add(), display(), clear(), encrypt(), decrypt(), merge(), countWords(), countChar(), countLines(), search(), countTimesWord(), turnUpper(), turnLower(), firstCaps();
+
+#endif
